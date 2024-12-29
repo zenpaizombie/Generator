ee06f3216310d611b24371c7675c32cede25640b
ZenpaiZombie <136229666+zenpaizombie@users.noreply.github.com>
2024-12-29 13:48:28 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 [
-  "",
-  "",
+  "https://raw.githubusercontent.com/zenpaizombie/Generator/refs/heads/main/Dockerfile",
+  "https://raw.githubusercontent.com/zenpaizombie/Generator/refs/heads/main/main.py",
   "",
   "",
   ""
