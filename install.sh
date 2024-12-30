b1fc8ae5094def4fcdcc84eadf0de6463bf603f1
ZenpaiZombie <136229666+zenpaizombie@users.noreply.github.com>
2024-12-30 22:55:00 +0600

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-[
-  "https://raw.githubusercontent.com/zenpaizombie/Generator/refs/heads/main/Dockerfile",
-  "https://raw.githubusercontent.com/zenpaizombie/Generator/refs/heads/main/main.py",
-  "",
-  "",
-  ""
-]
