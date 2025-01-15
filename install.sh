c7a4a728d273d89f53be4452fe4a48afbc8286a1
ZenpaiZombie <136229666+zenpaizombie@users.noreply.github.com>
2025-01-15 19:27:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ fi
 
 cd ~
 
-echo "Installing python3-pip and docker."
+echo "Installing python3, pip and docker."
 sudo apt update
-sudo apt install -y python3 pip docker.io
+sudo apt install python3 pip docker docker-compose -y
 echo Installed successfully
 
 echo "Writing Dockerfile..."
@@ -51,9 +51,9 @@ echo Downloaded successfully
 echo "Installing Python packages: discord and docker..."
 pip install discord.py docker
 echo "Please enter your Discord bot token, Make a bot at discord.dev and get the token, You dont need any intents:"
-read -r DISCORD_TOKEN
+read -r TOKEN
 echo "Updating main.py with the provided Discord token..."
-sed -i "s/TOKEN = ''/TOKEN = '$DISCORD_TOKEN'/" main.py
+sed -i "s/TOKEN = ''/TOKEN = 'TOKEN'/" main.py
 echo "Starting the Discord bot..."
 echo "To start the bot in the future, run: python3 main.py"
 python3 main.py
